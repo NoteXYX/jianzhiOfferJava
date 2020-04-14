cf2dbc1db7bc9fcb2119cf15d9f57ab5fed43976
NoteXYX <707997228@qq.com>
2020-04-14 09:47:52 +0800

    ok

--- src/com/jianzhi/Pack.java
+++ src/com/jianzhi/Pack.java
@@ -4,12 +4,16 @@ public class Pack {
     public static void main(String[] args) {
         int[] w = {2, 1, 3, 2};
         int[] v = {12, 10, 20, 15};
+        int[] m = {2, 3, 3, 6};
         Pack01 pack01 = new Pack01();
         System.out.println(pack01.knapSack(w, v, 5));
         System.out.println(pack01.youhua(w, v, 5));
         Packwq packwq = new Packwq();
         System.out.println(packwq.wqpack(w, v, 5));
         System.out.println(packwq.youhua(w, v, 5));
+        Packdc packdc = new Packdc();
+        System.out.println(packdc.dcpack(w, v, m, 5));
+        System.out.println(packdc.youhua(w, v, m, 5));
     }
 }
 class Pack01 {
@@ -48,7 +52,7 @@ class Packwq {
         for(int i = 0; i < v.length; i++) {
             for(int j = 0; j <= cap; j++) {
                 for(int k=0; k * w[i] <= j; k++)
-                    dp[i+1][j]=Math.max(dp[i+1][j],dp[i][j - k * w[i]] + k * v[i]);
+                    dp[i+1][j]=Math.max(dp[i+1][j], dp[i][j - k * w[i]] + k * v[i]);
             }
         }
         return dp[v.length][cap];
@@ -57,8 +61,32 @@ class Packwq {
         //dp[j] = max(dp[j], dp[j-w[i]]+v[i])
         int[] dp = new int[cap+1];
         for (int i = 0; i < v.length; i++) {
-            for (int j = w[i]; j <= cap; j++) {
+            for (int j = w[i]; j <= cap; j++) {    //从第一个可装的背包开始装
                 dp[j] = Math.max(dp[j], dp[j-w[i]] + v[i]);
+            }
+        }
+        return dp[cap];
+    }
+}
+class Packdc {
+    public int dcpack(int[] w, int[] v, int[] m, int cap) {
+        int[][] dp = new int[v.length+1][cap+1];
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j <= cap; j++) {
+                for (int k = 0; k<=m[i] && k*w[i]<=j ; k++) {
+                    dp[i+1][j] = Math.max(dp[i+1][j], dp[i][j - k * w[i]] + k * v[i]);
+                }
+            }
+        }
+        return dp[v.length][cap];
+    }
+    public int youhua(int[] w, int[] v, int[] m, int cap) {
+        int[] dp = new int[cap+1];
+        for (int i = 0; i < v.length; i++) {
+            int k = 1;
+            for (int j = w[i]; k<=m[i] && j<=cap ; j++) {
+                dp[j] = Math.max(dp[j], dp[j-w[i]] + v[i]);
+                k++;
             }
         }
         return dp[cap];
